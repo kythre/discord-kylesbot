8449e9a61495e322740a81c3a2e5a139ec8dd0fb
Kyle <thisisnotkylesemail@gmail.com>
2018-12-16 15:30:24 -0600

    delayed the persist by 250 ms to run after bots with autoroles

--- functions/persist.js
+++ functions/persist.js
@@ -1,16 +1,4 @@
-let cacheMember = function (guild, member) {
-  if (member.bot) {
-    return;
-  }
-
-  guild.settings.memberCache[member.id] = {
-    nick: member.nick,
-    roles: member.roles
-  };
-};
-
 module.exports = (bot) => {
-
   bot.registerCommandConfig({
     name: "persistroles",
     verbose: "role persist",
@@ -23,10 +11,21 @@ module.exports = (bot) => {
     setting: "persist.nick"
   });
 
+  let cacheMember = function (guild, member) {
+    if (member.bot) {
+      return;
+    }
+
+    bot.guildSettings[guild.id].memberCache[member.id] = {
+      nick: member.nick,
+      roles: member.roles
+    };
+  };
+
   bot.on("guildMemberRemove", cacheMember);
   bot.on("guildMemberUpdate", cacheMember);
 
-  bot.on("guildMemberAdd", (guild, member) => {
+  bot.on("guildMemberAdd", async (guild, member) => {
     if (member.bot) {
       return;
     }
@@ -34,15 +33,17 @@ module.exports = (bot) => {
     let guildSettings = bot.guildSettings[guild.id];
     let memberCache = guildSettings.memberCache[member.id];
 
-    if (memberCache && (guildSettings.persist.nick || guildSettings.persist.nick)) {
-      try {
-        member.edit({
-          roles: guildSettings.persist.roles ? memberCache.roles : member.roles,
-          nick: guildSettings.persist.nick ? memberCache.nick : member.nick
-        }, "Persist");
-      } catch (err) {
-        bot.error("Persists", err);
-      }
+    if (memberCache && (guildSettings.persist.nick || guildSettings.persist.roles)) {
+      await setTimeout(async () => {
+        try {
+          await member.edit({
+            roles: guildSettings.persist.roles ? memberCache.roles : member.roles,
+            nick: guildSettings.persist.nick ? memberCache.nick : member.nick
+          }, "Persist");
+        } catch (err) {
+          bot.error("Persists", err);
+        }
+      }, 250);
     }
   });
 };
