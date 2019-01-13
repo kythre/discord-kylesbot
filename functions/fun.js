8dd3fd687f6b11883481bb3dc3d9c336ea41515b
Kyle <thisisnotkylesemail@gmail.com>
2019-01-13 13:15:39 -0600

    added magic 8 ball command and changed say command description

--- functions/fun.js
+++ functions/fun.js
@@ -4,7 +4,7 @@ module.exports = (bot) => {
     category: "fun",
     info: {
       args: "[anything]",
-      description: "does nothing"
+      description: "says"
     },
     generator: (msg, args) => {
       msg.delete().catch(() => {
@@ -13,6 +13,41 @@ module.exports = (bot) => {
       msg.channel.createMessage(args.join(" ")).catch(() => {
         // fail
       });
+    }
+  });
+
+  let responses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ];
+
+  bot.registerCommand({
+    name: "m8b",
+    category: "fun",
+    info: {
+      args: "[none]",
+      description: "magic 8 ball"
+    },
+    generator: (msg) => {
+      bot.send(msg, responses[~~(Math.random() * responses.length)]);
     }
   });
 };
